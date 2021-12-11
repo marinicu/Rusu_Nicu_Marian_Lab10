f27341e34cbb34b41b41ef8924566bff7fe63e2e
Marian <Marian@DESKTOP-T8JCT4A>
2021-12-11 11:58:25 +0200

    Add project files.

---
+++ Rusu_Nicu_Marian_Lab10/Rusu_Nicu_Marian_Lab10/Models/ShopList.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+using System;
+
+namespace Rusu_Nicu_Marian_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
